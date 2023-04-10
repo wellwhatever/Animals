24edf566c61c383c715a65b9aac80e07f7026ac0
wellwhatever <petrov.oleksandr35@gmail.com>
2023-04-11 01:20:41 +0200

    feat: setupTimber

--- app/src/main/java/com/example/animals/MainActivity.kt
+++ app/src/main/java/com/example/animals/MainActivity.kt
@@ -5,15 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
 import com.example.animals.ui.theme.AnimalsTheme
+import timber.log.Timber
 
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupTimber()
         setContent {
             AnimalsTheme {
                 AnimalsAppStartRoute()
             }
+        }
+    }
+
+    private fun setupTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            // FIXME: plant release tree here, dummy for now
+            Timber.plant(Timber.DebugTree())
         }
     }
 }
