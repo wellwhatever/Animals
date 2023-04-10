b5924e2c157d5edcf4a4d6e0d4ce5cf9c79fb42b
wellwhatever <petrov.oleksandr35@gmail.com>
2023-04-10 20:38:21 +0200

    feat: add app build modules

---
+++ buildSrc/src/main/java/libs/BuildModules.kt
@@ -0,0 +1,11 @@
+package libs
+
+object BuildModules {
+    const val app = ":app"
+    const val coreNetwork = ":core:network"
+    const val coreCommon = ":core:common"
+    const val dataDogs = ":data:dogs"
+    const val domainDogs = ":domain:dogs"
+    const val featureDogs = ":feature:dogs"
+    const val coreCompose = ":core:compose"
+}
