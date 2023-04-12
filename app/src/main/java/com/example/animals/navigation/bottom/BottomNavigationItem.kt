691aaf3167628ca668f39d519e367ccacb2100f8
wellwhatever <petrov.oleksandr35@gmail.com>
2023-04-12 03:10:02 +0200

    refactor: remove useless navigation destination interface

--- app/src/main/java/com/example/animals/navigation/bottom/BottomNavigationItem.kt
+++ app/src/main/java/com/example/animals/navigation/bottom/BottomNavigationItem.kt
@@ -4,35 +4,30 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.ui.unit.dp
 import com.example.animals.R
-import com.example.common.AppNavigationDestination
 import com.example.dogs.navigation.DOG_FEED_ROUTE
 
 sealed class BottomNavigationItem(
     @StringRes val titleRes: Int,
     @DrawableRes var iconRes: Int,
-    override val route: String,
-    override val destination: String
-) : AppNavigationDestination {
+    val route: String
+) {
     object Dogs :
         BottomNavigationItem(
             titleRes = R.string.bottom_navigation_dogs_route_title,
             iconRes = R.drawable.ic_thumb_up,
-            route = DOG_FEED_ROUTE,
-            destination = "dogs_destination"
+            route = DOG_FEED_ROUTE
         )
 
     object Cats : BottomNavigationItem(
         titleRes = R.string.bottom_navigation_cats_route_title,
         iconRes = R.drawable.ic_thumb_down,
-        route = "cats_route",
-        destination = "cats_destination"
+        route = "cats_route"
     )
 
     object Other : BottomNavigationItem(
         titleRes = R.string.bottom_navigation_other_route_title,
         iconRes = R.drawable.ic_wrong_location,
-        route = "other_route",
-        destination = "other_destination"
+        route = "other_route"
     )
 
     companion object {
